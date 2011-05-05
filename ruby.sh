a7db6cc685b53bf706ab2a5d1fda7ffde93bf08c
Matt Blair <me@matthewblair.net>
2011-05-05 18:02:50 -0400

    rvm does this anyway

--- ruby.sh
+++ ruby.sh
@@ -5,7 +5,6 @@ set -x
 curl -s https://rvm.beginrescueend.com/install/rvm -o rvm-installer
 chmod +x rvm-installer
 ./rvm-installer --version latest
-echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"  # This loads RVM into a shell session.' >> ~/.bash_profile
 . "$HOME/.rvm/scripts/rvm"
 
 echo 'install: --no-rdoc --no-ri' >> ~/.gemrc
