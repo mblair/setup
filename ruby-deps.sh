95cab787522fc93d287d05553395b127039a16c6
Matthew Blair <me@matthewblair.net>
2011-05-23 08:04:59 -0700

    Damned privileges.

--- ruby-deps.sh
+++ ruby-deps.sh
@@ -4,3 +4,10 @@ set -e
 set -x
 
 apt-get install -y bison libxml2-dev libxslt1-dev autoconf libreadline-dev
+
+#The RVM installer needs Git.
+if grep 'lucid' /etc/lsb-release 1>/dev/null; then
+		apt-get -y install git-core
+else
+		apt-get -y install git
+fi
