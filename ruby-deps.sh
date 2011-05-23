97fc9a9bf70d629570756deffe283cc521baeab3
Matthew Blair <me@matthewblair.net>
2011-05-23 08:22:50 -0700

    Need libssl-dev for REE.

--- ruby-deps.sh
+++ ruby-deps.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-apt-get install -y bison libxml2-dev libxslt1-dev autoconf libreadline-dev
+apt-get install -y bison libxml2-dev libxslt1-dev autoconf libreadline-dev libssl-dev
 
 #The RVM installer needs Git.
 if grep 'lucid' /etc/lsb-release 1>/dev/null; then
