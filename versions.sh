f617e8b49e47e9624739c457a931e8e85ac0f963
Matt Blair <me@matthewblair.net>
2011-10-10 23:26:32 -0700

    updated versions

--- versions.sh
+++ versions.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-GIT_VER=1.7.6.4 #http://git-scm.com/
+GIT_VER=1.7.7 #http://git-scm.com/
 SQLITE_VER=3070800 #http://www.sqlite.org/download.html
 PYTHON_VER=2.7.2 #http://python.org/download/
 PYTHON_SHORT_VER="`echo $PYTHON_VER | cut -f1 -d"."`.`echo $PYTHON_VER | cut -f2 -d"."`"
 PYTHON3_VER=3.2.2 #http://python.org/download/
 PYTHON3_SHORT_VER="`echo $PYTHON3_VER | cut -f1 -d"."`.`echo $PYTHON3_VER | cut -f2 -d"."`"
-HG_VER=1.9.2 #http://mercurial.selenic.com/downloads/
+HG_VER=1.9.3 #http://mercurial.selenic.com/downloads/
 VIM_VER=7.3 #http://www.vim.org/download.php
 PERCONA_DEFAULT_VER=5.5
 PERCONA_MYSQL_51_SHORT_VER=5.1
@@ -17,7 +17,7 @@ PERCONA_MYSQL_55_VER=5.5.15 #http://www.percona.com/software/percona-server/down
 PERCONA_55_VER=21.0 #http://www.percona.com/software/percona-server/downloads/
 POSTGRES_VER=9.1.1 #http://www.postgresql.org/ftp/source/
 LLVM_VER=2.9 #http://llvm.org/releases/
-NGINX_VER=1.0.6 #http://nginx.org/en/download.html
+NGINX_VER=1.0.8 #http://nginx.org/en/download.html
 NODEJS_VER=0.4.12 #http://nodejs.org/dist/
 PIDGIN_VER=2.10.0 #http://pidgin.im/download/source/
 
