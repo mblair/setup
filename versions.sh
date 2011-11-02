bb5fa8d6b4a020978010f545adbb5e653236d5e6
Matt Blair <me@matthewblair.net>
2011-11-02 16:06:36 -0700

    New sqlite, Mercurial, nginx.

--- versions.sh
+++ versions.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 GIT_VER=1.7.7 #http://git-scm.com/
-SQLITE_VER=3070800 #http://www.sqlite.org/download.html
+SQLITE_VER=3070900 #http://www.sqlite.org/download.html
 PYTHON_VER=2.7.2 #http://python.org/download/
 PYTHON_SHORT_VER="`echo $PYTHON_VER | cut -f1,2 -d"."`"
 PYTHON3_VER=3.2.2 #http://python.org/download/
 PYTHON3_SHORT_VER="`echo $PYTHON3_VER | cut -f1,2 -d"."`"
-HG_VER=1.9.3 #http://mercurial.selenic.com/downloads/
+HG_VER=2.0 #http://mercurial.selenic.com/downloads/
 VIM_VER=7.3 #http://www.vim.org/download.php
 PERCONA_DEFAULT_VER=5.5
 PERCONA_MYSQL_51_VER=5.1.59 #http://www.percona.com/software/percona-server/downloads/
@@ -17,7 +17,7 @@ PERCONA_MYSQL_55_SHORT_VER="`echo $PERCONA_MYSQL_55_VER | cut -f1,2 -d "."`"
 PERCONA_55_VER=22.0 #http://www.percona.com/software/percona-server/downloads/
 POSTGRES_VER=9.1.1 #http://www.postgresql.org/ftp/source/
 LLVM_VER=2.9 #http://llvm.org/releases/
-NGINX_VER=1.0.8 #http://nginx.org/en/download.html
+NGINX_VER=1.0.9 #http://nginx.org/en/download.html
 NODEJS_VER=0.4.12 #http://nodejs.org/dist/
 PIDGIN_VER=2.10.0 #http://pidgin.im/download/source/
 
