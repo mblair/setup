692507cf13014320c0b5bde11e18182803bc378c
Matt Blair <me@matthewblair.net>
2011-11-24 00:19:45 -0500

    new versions

--- versions.sh
+++ versions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-GIT_VER=1.7.7 #http://git-scm.com/
+GIT_VER=1.7.7.4 #http://git-scm.com/
 SQLITE_VER=3070900 #http://www.sqlite.org/download.html
 PYTHON_VER=2.7.2 #http://python.org/download/
 PYTHON_SHORT_VER="`echo $PYTHON_VER | cut -f1,2 -d"."`"
@@ -12,13 +12,13 @@ PERCONA_DEFAULT_VER=5.5
 PERCONA_MYSQL_51_VER=5.1.59 #http://www.percona.com/software/percona-server/downloads/
 PERCONA_MYSQL_51_SHORT_VER="`echo $PERCONA_MYSQL_51_VER | cut -f1,2 -d"."`"
 PERCONA_51_VER=13.0 #http://www.percona.com/software/percona-server/downloads/
-PERCONA_MYSQL_55_VER=5.5.16 #http://www.percona.com/software/percona-server/downloads/
+PERCONA_MYSQL_55_VER=5.5.17 #http://www.percona.com/software/percona-server/downloads/
 PERCONA_MYSQL_55_SHORT_VER="`echo $PERCONA_MYSQL_55_VER | cut -f1,2 -d "."`"
-PERCONA_55_VER=22.0 #http://www.percona.com/software/percona-server/downloads/
+PERCONA_55_VER=22.1 #http://www.percona.com/software/percona-server/downloads/
 POSTGRES_VER=9.1.1 #http://www.postgresql.org/ftp/source/
 LLVM_VER=2.9 #http://llvm.org/releases/
-NGINX_VER=1.0.9 #http://nginx.org/en/download.html
-NODEJS_VER=0.4.12 #http://nodejs.org/dist/
+NGINX_VER=1.0.10 #http://nginx.org/en/download.html
+NODEJS_VER=0.6.2 #http://nodejs.org/dist/
 PIDGIN_VER=2.10.0 #http://pidgin.im/download/source/
 
 IRC_PASS=changeme
